09b787a784ad103bfeffe02f78285d14eebd2318
caoshuiming <2639067440@qq.com>
2021-01-21 15:22:28 +0800

    new project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>小米9</span>
+    数量：{{num}}
+    已售：{{$store.getters.sale}}
+    <br>
+    <button :disabled="!num" @click="$store.commit('reduce')">购买1个</button>
+    <button :disabled="!num" @click="$store.commit('doubleReduce')">购买2个</button>
+    <button :disabled="!num" @click="$store.dispatch('doubleReduce2')">网络卡，但还是要买，买双倍！</button>
+    <p>
+      <input type="text" v-model="$store.state.buyNum">
+      <button :disabled="!num" @click="$store.commit('buy',$store.state.buyNum)">买</button>
+    </p>
+    <hr>
+    <p>{{count}}</p>
+    <p>{{$store.getters.doubleCount}}</p>
+    <p>
+      <button @click="$store.commit('increment')">加一</button>
+      <button @click="$store.dispatch('increment2')">延时加一</button>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed:{
+    count(){
+      return this.$store.state.count
+    },
+    num(){
+      return this.$store.state.num
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,67 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
+// const proceed = {
+//   install(){
+//     alert('Hello')
+//   }
+// }
+// Vue.use(proceed)
+// Vue.use执行install方法
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state:{
+    count:0,
+    num:100,
+    buyNum:0
+  },
+  mutations:{
+    increment(state){
+      state.count++
+    },
+    reduce(state){
+      state.num--;
+    },
+    doubleReduce(state){
+      state.num-=2
+    },
+    buy(state,n){
+      if(n>100){
+        n=100;
+      }
+      if(n<0){
+        n=0;
+      }
+      state.num-=n;
+    }
+  },
+  actions:{
+    increment2(action){
+      setTimeout(() => {
+        action.commit('increment')
+      },1000)
+    },
+    doubleReduce2(action){
+      setTimeout(() => {
+        action.commit('doubleReduce')
+      },1000)
+    }
+  },
+  getters:{
+    doubleCount(state){
+      return state.count*2;
+    },
+    sale(state){
+      return 100-state.num;
+    }
+  }
+})
+
 new Vue({
+  // ES6property对象的简写
+  store,
   render: h => h(App),
 }).$mount('#app')
